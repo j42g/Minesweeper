d636a7c554201c731a7871fad1e97a43d556c147
j42g <jasi.guenther@gmail.com>
2022-11-26 17:56:51 +0100

    get Game from screenshot

--- src/Starter/Starter.java
+++ src/Starter/Starter.java
@@ -2,13 +2,7 @@ package Starter;
 
 import Input.InputGame;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-
 public class Starter {
-
 
     public static void main(String[] args) {
 
