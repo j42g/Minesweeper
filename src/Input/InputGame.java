acd8be71cdd08b2b02218b50dea766fc8f23027f
j42g <jasi.guenther@gmail.com>
2022-12-05 18:41:22 +0100

    more adapdably

--- src/Input/InputGame.java
+++ src/Input/InputGame.java
@@ -9,16 +9,26 @@ import java.awt.*;
 import java.awt.event.InputEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.ArrayList;
 
 public class InputGame {
 
-    private static final int[] coordsULC = {1022 - 1, 310 - 1};
-    private static final int[] coordsLRC = {1741 + 1, 693 + 1};
+    private static final int WIDTH = 30;
+    private static final int HEIGHT = 16;
+    private static final int SQUARELENGTH = 24;
+    private static final int TOTALBOMBS = 99;
+    private static final int[] URCorner = new int[]{1022, 310};
 
-    private static final int[] coordsLAPULC = {511 - 1, 311 - 1};
-    private static final int[] coordsLAPLRC = {1229 + 1, 693 + 1};
-    private static final Rectangle section = new Rectangle(coordsLAPULC[0], coordsLAPULC[1],
-            coordsLAPLRC[0] - coordsLAPULC[0], coordsLAPLRC[1] - coordsLAPULC[1]);
+    // hard/evil etc : {1055, 310}
+    // hard/evil etc : {1022, 310}
+    // 80*40 : {422, 360}
+    // LAPTOP : {511, 311}
+
+    //
+    private static final int[] coordsULC = {URCorner[0] - 1, URCorner[1] - 1};
+    private static final int[] coordsLRC = {URCorner[0] + SQUARELENGTH*WIDTH + 1, URCorner[1] + SQUARELENGTH*HEIGHT + 1};
+    private static final Rectangle section = new Rectangle(coordsULC[0], coordsULC[1],
+            coordsLRC[0] - coordsULC[0], coordsLRC[1] - coordsULC[1]);
 
     public static void solve(){
         for(int i = 3; i > 0; i--){
@@ -36,17 +46,21 @@ public class InputGame {
         } catch (AWTException e) {
             throw new RuntimeException(e);
         }
-        for(int j = 0; j < 70; j++){
-            a = new Grid(takeScreenshot());
-            if(a.getRemainingBombCount() == 0){
-                break;
+        for(int j = 0; j < 100; j++){
+            a = new Grid(WIDTH, HEIGHT, TOTALBOMBS, takeScreenshot());
+            b = new DetSolve(a);
+            //a.print();
+            for(Move i : b.giveBestMoves()) {
+                if (i.shouldMark()) {
+                    a.move(i);
+                }
             }
             b = new DetSolve(a);
-            a.print();
-            System.out.println("Screenshot:\t" + (j + 1));
-            for(Move i : b.giveBestMoves()){
-                //System.out.println(i);
-                c.mouseMove(coordsLAPULC[0] + 24*i.getX() + 12, coordsLAPULC[1] + 24*i.getY() + 12);
+            for(Move i : b.giveBestMovesEqOnly()){
+                if(i.shouldMark()){
+                    continue;
+                }
+                c.mouseMove(coordsULC[0] + SQUARELENGTH*i.getX() + 12, coordsULC[1] + SQUARELENGTH*i.getY() + 12);
                 if(i.shouldMark()){
                     c.mousePress(InputEvent.BUTTON3_DOWN_MASK);
                     c.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
@@ -55,29 +69,38 @@ public class InputGame {
                     c.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
                 }
                 //a.print();
+                if(shouldStop(a) && false){
+                    return;
+                }
                 try {
-                    Thread.sleep(10);
+                    Thread.sleep(1);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
             try {
-                Thread.sleep(50);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
     }
 
-    public static void bestMoves(){
-        Grid a = new Grid(takeScreenshot());
-        a.print();
-        DetSolve b = new DetSolve(a);
-        for(Move i : b.giveBestMoves()){
-            System.out.println(i);
-        }
+    private static boolean shouldStop(Grid grid){
+        // expand with other logic
+        return grid.getRemainingBombCount() <= 1;
     }
 
+    public static void bestMoves(){
+        Grid a = new Grid(WIDTH, HEIGHT, TOTALBOMBS, takeScreenshot());
+        a.print();
+        DetSolve b = new DetSolve(a);
+        for(Move i : b.giveBestMovesEqOnly()){
+            if(!i.shouldMark()) a.move(i);
+            if(!i.shouldMark()) System.out.println(i);
+        }
+        a.print();
+    }
 
     private static BufferedImage takeScreenshot(){
         try {
