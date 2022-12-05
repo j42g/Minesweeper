a94a9b2abd2991f471c401f37b521e09b60a0ace
j42g <jasi.guenther@gmail.com>
2022-12-05 18:42:44 +0100

    -

--- src/Starter/Starter.java
+++ src/Starter/Starter.java
@@ -1,18 +1,13 @@
 package Starter;
 
 import Game.Grid;
-import Input.HTTPGame;
 import Input.InputGame;
 import Solver.DetSolve;
 
 public class Starter {
     public static void main(String[] args) {
-        InputGame.solve();
-        try {
-            HTTPGame.getHTML("https://minesweeper.online/game/1783966242");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+
+        InputGame.bestMoves();
 
     }
 }
