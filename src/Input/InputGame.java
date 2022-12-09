0777194d490e28a23eade80fd1bb1c0ffd0c8884
j42g <jasi.guenther@gmail.com>
2022-12-09 21:11:47 +0100

    modularisierung + generalisierung

--- src/Input/InputGame.java
+++ src/Input/InputGame.java
@@ -9,28 +9,31 @@ import java.awt.*;
 import java.awt.event.InputEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.ArrayList;
+import java.util.Stack;
 
 public class InputGame {
 
-    private static final int WIDTH = 30;
-    private static final int HEIGHT = 16;
-    private static final int SQUARELENGTH = 24;
-    private static final int TOTALBOMBS = 99;
-    private static final int[] URCorner = new int[]{1022, 310};
+    private static final int WIDTH = 42;
+    private static final int HEIGHT = 42;
+    private static final int SQUARELEN = 16;
+    private static final int TOTALBOMBS = 420;
+    private static final int[] URCorner = new int[]{16, 101};
 
     // hard/evil etc : {1055, 310}
     // hard/evil etc : {1022, 310}
     // 80*40 : {422, 360}
     // LAPTOP : {511, 311}
 
-    //
+    // MinesweeperX Curr : {16, 101}
+
+    // derived
+    private static final int HALFSQUARELEN = SQUARELEN / 2;
     private static final int[] coordsULC = {URCorner[0] - 1, URCorner[1] - 1};
-    private static final int[] coordsLRC = {URCorner[0] + SQUARELENGTH*WIDTH + 1, URCorner[1] + SQUARELENGTH*HEIGHT + 1};
+    private static final int[] coordsLRC = {URCorner[0] + SQUARELEN *WIDTH + 1, URCorner[1] + SQUARELEN *HEIGHT + 1};
     private static final Rectangle section = new Rectangle(coordsULC[0], coordsULC[1],
             coordsLRC[0] - coordsULC[0], coordsLRC[1] - coordsULC[1]);
 
-    public static void solve(){
+    public static void solve(boolean shouldMark) {
         for(int i = 3; i > 0; i--){
             System.out.print("\rStarting in " + i);
             try {
@@ -46,28 +49,20 @@ public class InputGame {
         } catch (AWTException e) {
             throw new RuntimeException(e);
         }
-        for(int j = 0; j < 100; j++){
-            a = new Grid(WIDTH, HEIGHT, TOTALBOMBS, takeScreenshot());
+        for(int j = 0; j < 200; j++){
+            a = new Grid(WIDTH, HEIGHT, TOTALBOMBS, SQUARELEN, takeScreenshot());
+            //System.out.println(a);
             b = new DetSolve(a);
             //a.print();
-            for(Move i : b.giveBestMoves()) {
-                if (i.shouldMark()) {
-                    a.move(i);
-                }
+            if(!shouldMark){
+                markInternal(a, b);
+                b = new DetSolve(a);
             }
-            b = new DetSolve(a);
-            for(Move i : b.giveBestMovesEqOnly()){
-                if(i.shouldMark()){
+            for(Move i : b.giveBestMoves()){
+                if(i.shouldMark() && !shouldMark){
                     continue;
                 }
-                c.mouseMove(coordsULC[0] + SQUARELENGTH*i.getX() + 12, coordsULC[1] + SQUARELENGTH*i.getY() + 12);
-                if(i.shouldMark()){
-                    c.mousePress(InputEvent.BUTTON3_DOWN_MASK);
-                    c.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
-                } else {
-                    c.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-                    c.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-                }
+                click(c, i);
                 //a.print();
                 if(shouldStop(a) && false){
                     return;
@@ -86,13 +81,40 @@ public class InputGame {
         }
     }
 
+    private static void markInternal(Grid grid, DetSolve solver){
+        Stack<Move> moves = solver.giveBestMoves();
+        for(Move i : moves) {
+            if (i.shouldMark()) {
+                grid.move(i);
+            }
+        }
+        DetSolve solv2 = new DetSolve(grid);
+        moves = solv2.giveBestMovesEqOnly();
+        for(Move i : moves) {
+            if (i.shouldMark()) {
+                grid.move(i);
+            }
+        }
+    }
+
+    private static void click(Robot clicker, Move move){
+        clicker.mouseMove(coordsULC[0] + SQUARELEN *move.getX() + HALFSQUARELEN, coordsULC[1] + SQUARELEN *move.getY() + HALFSQUARELEN);
+        if(move.shouldMark()){
+            clicker.mousePress(InputEvent.BUTTON3_DOWN_MASK);
+            clicker.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+        } else {
+            clicker.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            clicker.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        }
+    }
+
     private static boolean shouldStop(Grid grid){
         // expand with other logic
         return grid.getRemainingBombCount() <= 1;
     }
 
     public static void bestMoves(){
-        Grid a = new Grid(WIDTH, HEIGHT, TOTALBOMBS, takeScreenshot());
+        Grid a = new Grid(WIDTH, HEIGHT, TOTALBOMBS, SQUARELEN, takeScreenshot());
         a.print();
         DetSolve b = new DetSolve(a);
         for(Move i : b.giveBestMovesEqOnly()){
